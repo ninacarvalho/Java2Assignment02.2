95d345b9af6dc97393a7d4e354b4b03256db93b7
Desmond <142507179+desmondsyu@users.noreply.github.com>
2024-02-07 13:34:54 -0500

    Update Task02.java
    Add comments

--- src/task2/Task02.java
+++ src/task2/Task02.java
@@ -7,10 +7,12 @@ import java.util.Scanner;
 public class Task02 {
     public static void main(String[] args) {
 
+        // CREATE ARRAY OF 5 ELEMENTS
         Scanner scan = new Scanner(System.in);
         double[] numberList = new double[5];
         int listIndex = 0;
 
+        // PROMPT USER TO ENTER VALUE OF ARRAY
         while (listIndex < numberList.length) {
             System.out.printf("Input #%d number for list\n", listIndex + 1);
             try {
@@ -22,15 +24,20 @@ public class Task02 {
             }
         }
 
+        // PROMPT USER ENTER OCCURRENCES CHECK NUMBER
         System.out.println("Which value do you want to check for occurrences?");
         double value = scan.nextDouble();
 
+        // USING LAMBDA EXPRESSION TO RETURN MAX, MIN, SUM, AVG
         double max = GET_MAX.apply(numberList);
         double min = GET_MIN.apply(numberList);
         double sum = GET_SUM.apply(numberList);
         double avg = GET_AVG.apply(numberList);
+
+        // USING LAMBDA EXPRESSION METHOD TO RETURN OCCURRANCES
         int count = (int) counter(value).apply(numberList);
 
+        // PRINT THE OUTPUT
         System.out.printf("Maixmum value: %.2f;\n" +
                 "Minimum value: %.2f;\n" +
                 "Sum of all values: %.2f;\n" +
@@ -38,11 +45,13 @@ public class Task02 {
                 "Number of occurrences of %.2f: %d;\n", max, min, sum, avg, value, count);
     }
 
+    // DEFINE VARIABLES OF ArrayProcessor
     public static final ArrayProcessor GET_MAX = a -> Arrays.stream(a).max().orElse(Double.NaN);
     public static final ArrayProcessor GET_MIN = a -> Arrays.stream(a).min().orElse(Double.NaN);
     public static final ArrayProcessor GET_SUM = a -> Arrays.stream(a).sum();
     public static final ArrayProcessor GET_AVG = a -> Arrays.stream(a).average().orElse(Double.NaN);
 
+    // DEFINE FUNCTION RETURNS AN ArrayProcessor THAT COUNTS THE OCCURRANCES 
     public static ArrayProcessor counter(double value) {
         return a -> Arrays.stream(a).filter(d -> d == value).count();
     }
